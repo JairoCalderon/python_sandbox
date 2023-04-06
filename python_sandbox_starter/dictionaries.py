a5f63fbcd532bcd5e7ee65cf3174eb26896ce278
JairoCalderon <j.calderoneng@gmail.com>
2023-04-05 21:46:04 -0400

    dictionaries

--- python_sandbox_starter/dictionaries.py
+++ python_sandbox_starter/dictionaries.py
@@ -1,2 +1,31 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
 # Read more about dictionaries at https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+
+#similar to obejct literal in JavaScript
+
+#creatign a dictionary
+person = {
+    'firstName' : 'John',
+    'lastName' : "Doe",
+    'age' : 30
+}
+
+
+#using a constructor
+# person2 = dict(firstName='Sara', lastName='Williams')
+
+
+# print(person2, type(person2))
+
+#getting a value
+print(person['firstName'])
+print(person.get('lastName'))
+
+#add key/value
+person['phone'] = '6478524152'
+
+#getting dict keys
+print(person.keys())
+
+#getting dict items
+print(person.items())
--- python_sandbox_starter/tuples_sets.py
+++ python_sandbox_starter/tuples_sets.py
@@ -1,5 +1,38 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
  
+#creatign a tuple
+fruits = ("apples", "oranges", "grapes")
+
+#singles values needs trailing comma
+fruits2 = ("oranges",)
+
+#getting a value from a tupe
+print(fruits[1])
+
+#em Tuples can not change values, they are unschangechable
+#fruits[0]= "watermelon" this won't work
+
 
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+
+#creatign a set
+fruitsSet={"apples","oranges", "mangos"}
+
+#check if in the set
+print("apples" in fruitsSet)
+
+#adding to the set
+fruitsSet.add("grapes")
+
+#removing from the set
+fruitsSet.remove("mangos")
+
+#adding duplicate velues it won't work. It won't give an error, it'll just not add the extra duplicated value
+fruitsSet.add("apples")
+
+#clearing the set
+# fruitsSet.clear()
+
+#deleting a set
+# del fruitsSet
